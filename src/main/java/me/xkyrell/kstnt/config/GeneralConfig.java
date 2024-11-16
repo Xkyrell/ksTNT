52b6e3b6ab151d64bbbc0e45aa9c593faa08d628
xKyrell <vovadragan03@gmail.com>
2024-11-16 21:39:49 +0200

    Add Config, GeneralConfig and LanguageConfig

---
+++ src/main/java/me/xkyrell/kstnt/config/GeneralConfig.java
@@ -0,0 +1,10 @@
+package me.xkyrell.kstnt.config;
+
+import org.bukkit.plugin.Plugin;
+
+public class GeneralConfig extends Config {
+
+    public GeneralConfig(Plugin plugin) {
+        super(plugin, "config");
+    }
+}
---
+++ src/main/java/me/xkyrell/kstnt/config/LanguageConfig.java
@@ -0,0 +1,10 @@
+package me.xkyrell.kstnt.config;
+
+import org.bukkit.plugin.Plugin;
+
+public class LanguageConfig extends Config {
+
+    LanguageConfig(Plugin plugin) {
+        super(plugin, "language");
+    }
+}
